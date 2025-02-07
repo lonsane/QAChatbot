650046f830e8cd11e61add1cfb586a0a15a360d1
onkar lonsane <lonsaneonkar@gmail.com>
2025-02-07 15:27:38 +0530

    first chatbot

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QAChatbot',
+    version= '0.0.1',
+    author= 'Onkar Lonsane',
+    author_email= 'lonsaneonkar@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
